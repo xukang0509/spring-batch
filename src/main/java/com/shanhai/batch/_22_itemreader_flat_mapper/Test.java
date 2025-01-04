615f423fd8d485f6de0ac27c7173342722fda005
shanhai <shanhai@git.com>
2025-01-05 00:22:44 +0800

    test

---
+++ src/main/java/com/shanhai/batch/_22_itemreader_flat_mapper/Test.java
@@ -0,0 +1,12 @@
+package com.shanhai.batch._22_itemreader_flat_mapper;
+
+/**
+ * @description:
+ * @author: xu
+ * @date: 2025/1/5 0:21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
